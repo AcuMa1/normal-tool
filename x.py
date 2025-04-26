60871d70ca453a49537c1707d7362e278d4178d9
Acuma 🌹 <ang089afk@gmail.com>
2025-04-26 19:43:28 +0100

    Create x.py

---
+++ x.py
@@ -0,0 +1,28 @@
+from scapy.all import *
+import random
+import time
+
+def generate_random_ip():
+    return '.'.join(str(random.randint(0, 255)) for _ in range(4))
+
+def generate_random_port():
+    return random.randint(1024, 65535)
+
+def send_syn_packet(target_ip, target_port):
+    ip = IP(dst=target_ip)
+    syn = TCP(dport=target_port, flags="S", seq=random.randint(1000, 9000))
+    packet = ip/syn
+    send(packet, verbose=False)
+
+def load_test():
+    target_ip = generate_random_ip()
+    target_port = generate_random_port()
+    print(f"بدأ الاختبار: إرسال حزم إلى {target_ip} على المنفذ {target_port}")
+    
+    for i in range(100):
+        send_syn_packet(target_ip, target_port)
+        time.sleep(0.1)
+    
+    print("انتهى الاختبار!")
+
+load_test()
