d13f702b0471faa93dae900abf276ad705312a82
Acuma 🌹 <ang089afk@gmail.com>
2025-04-27 03:51:31 +0100

    Update x.py

--- x.py
+++ x.py
@@ -10,7 +10,7 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 def generate_random_string(length):
     letters_and_digits = string.ascii_letters + string.digits
     return ''.join(random.choice(letters_and_digits) for i in range(length))
-fk = generate_random_string(1000)
+fk = generate_random_string(29699)
 print(fk)
 def GenLogin():
     url = 'https://backend.kibomodz.online/api/users/login'
@@ -22,7 +22,7 @@ def GenLogin():
         'Referer': 'https://kibomodz.online/auth/login'
     }
     data = {
-        'email': generate_random_string(13) + "@gmail.com",
+        'email': fk + "@gmail.com",
         'password': fk,
         "turnstileToken": fk
     }
